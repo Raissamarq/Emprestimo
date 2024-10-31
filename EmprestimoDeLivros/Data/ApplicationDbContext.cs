586172e7dae4c2a09bbfe6764a40db36d8d5e0a1
RaissaMarquesB <raissamarquesb@gmail.com>
2024-10-31 15:59:32 -0300

    Ignorar arquivos de configuração do Visual Studio

---
+++ EmprestimoDeLivros/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EmprestimoDeLivros.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace EmprestimoDeLivros.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        //construtor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }  
+
+    }
+}
+
---
+++ EmprestimoDeLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoDeLivros.Models
+{
+    public class EmprestimosModel
+    {
+
+        //Atalho: prop + 2X Tab = criar propriedade
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do Recebedor!")]
+        public string Recebedor { get; set; }
+        [Required(ErrorMessage = "Digite o nome do Fornecedor!")]
+        public string Fornecedor { get; set; }
+        [Required(ErrorMessage = "Digite o nome do Livro!")]
+        public string LivroEmprestado { get; set; }
+
+        // Datetime.now = horario atual
+        public DateTime dataUltimaAtualizacao { get; set; } = DateTime.Now;
+
+    }
+}
